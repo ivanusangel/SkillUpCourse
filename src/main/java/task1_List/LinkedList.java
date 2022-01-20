447db808b2998f70e22a05e701d717b537933629
Ivan Smirnov <ivanus@ua.fm>
2022-01-20 11:54:23 +0200

    LinkedList part

--- src/main/java/task1_List/LinkedList.java
+++ src/main/java/task1_List/LinkedList.java
@@ -1,24 +1,69 @@
 package task1_List;
 
 public class LinkedList implements List {
+
+    private class Node {
+        Node prev;
+        Node next;
+        Object value;
+
+        public Node(Object value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size = 0;
+
+    public LinkedList() {
+    }
+
     @Override
     public void add(Object value) {
-
+        add(value, size);
     }
 
     @Override
     public void add(Object value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ". Expected between 0 and " + size);
+        }
+        Node newNode = new Node(value);
+        if (index == 0) {
+            if (first != null) {
+                first.prev = newNode;
+            }
+            newNode.next = first;
+            first = newNode;
+        }
+        if (index == size) {
+            if (last != null) {
+                last.next = newNode;
+            }
+            newNode.prev = last;
+            last = newNode;
+        }
+        if (index > 0 && index < size) {
+            Node currentNode = getNode(index);
+            newNode.next = currentNode;
+            newNode.prev = currentNode.prev;
+            currentNode.prev = newNode;
+        }
+        size++;
     }
 
     @Override
     public Object remove(int index) {
-        return null;
+        Node currentNode = getNode(index);
+        currentNode.prev.next = currentNode.next;
+        currentNode.next.prev = currentNode.prev;
+        return currentNode.value;
     }
 
     @Override
     public Object get(int index) {
-        return null;
+        return getNode(index).value;
     }
 
     @Override
@@ -27,18 +72,20 @@ public class LinkedList implements List {
     }
 
     @Override
-    public void clear() {
-
+    public void clear() { //can be memory leak? Should I clean all elements?
+        first = null;
+        last = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -54,6 +101,25 @@ public class LinkedList implements List {
     @Override
     public int lastIndexOf(Object value) {
         return 0;
+    }
+
+    private Node getNode(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException("Index: " + index + ". Expected between 0 and " + (size - 1));
+        }
+        Node currentNode;
+        if (index <= size / 2) {
+            currentNode = first;
+            for (int i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+        } else {
+            currentNode = last;
+            for (int i = size; i > index; i--) {
+                currentNode = currentNode.prev;
+            }
+        }
+        return currentNode;
     }
 
     @Override
