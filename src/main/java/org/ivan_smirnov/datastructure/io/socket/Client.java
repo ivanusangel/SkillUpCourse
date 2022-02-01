0bddac9d9a04036f9593d94ce2d23330f9683486
Ivan Smirnov <ivanus@ua.fm>
2022-02-01 13:51:03 +0200

    Socket task: Server and Client done

---
+++ src/main/java/org/ivan_smirnov/datastructure/io/socket/Client.java
@@ -0,0 +1,20 @@
+package org.ivan_smirnov.datastructure.io.socket;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("localhost", 3000);
+             InputStream inputStream = new BufferedInputStream(socket.getInputStream());
+             OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream())
+        ) {
+            outputStream.write("Hello from client".getBytes());
+            outputStream.flush();
+            byte[] buffer = new byte[1024];
+            int count = inputStream.read(buffer);
+            System.out.println(new String(buffer, 0, count));
+        }
+    }
+}
---
+++ src/main/java/org/ivan_smirnov/datastructure/io/socket/Server.java
@@ -0,0 +1,29 @@
+package org.ivan_smirnov.datastructure.io.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        try (
+                ServerSocket serverSocket = new ServerSocket(3000)
+        ) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        InputStream inputStream = new BufferedInputStream(socket.getInputStream());
+                        OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream())
+                ) {
+                    byte[] buffer = new byte[1024];
+                    int count = inputStream.read(buffer);
+                    System.out.println(new String(buffer, 0, count));
+                    outputStream.write("echo ".getBytes(StandardCharsets.UTF_8));
+                    outputStream.write(buffer);
+                    outputStream.flush();
+                }
+            }
+        }
+    }
+}
